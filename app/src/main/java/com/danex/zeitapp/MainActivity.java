cf58877f1676d7371fcafada84761206c03b2093
Lupu Daniel <daniellupu10@yahoo.com>
2019-10-27 12:53:19 +0100

    Buttons für das Menü wurden geändert

--- app/src/main/java/com/danex/zeitapp/MainActivity.java
+++ app/src/main/java/com/danex/zeitapp/MainActivity.java
@@ -2,13 +2,31 @@ package com.danex.zeitapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends Activity implements View.OnClickListener {
+    Button b1=null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b1=findViewById(R.id.button2);
+        b1.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent=new Intent(this,Start.class);
+        startActivityForResult(intent,123);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent){
+
     }
 }
